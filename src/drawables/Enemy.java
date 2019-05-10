e5acdab0c711f4100fd4e3f3d966c9f86d75f598
wilsonta <wilsonta@rose-hulman.edu>
2019-05-09 20:34:27 -0400

    added draw and update to enemy class

--- src/drawables/Enemy.java
+++ src/drawables/Enemy.java
@@ -1,5 +1,8 @@
 package drawables;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
 /**
  * The Enemy class for the arcade game.
  * 
@@ -13,8 +16,33 @@ package drawables;
  */
 
 public abstract class Enemy extends Character{
+	private int height;
+	private int width;
+	
 
 	public Enemy(double x, double y, double speed, double fallSpeed, double jumpSpeed) {
 		super(x, y, speed, fallSpeed, jumpSpeed);
+		this.height=40;
+		this.width=20;
+	}
+	
+	@Override
+	public void draw(Graphics2D g2) {
+		g2.translate(this.x, this.y);
+		g2.setColor(Color.RED);
+		g2.fillOval((int)this.x, (int)this.y, this.width, this.height);
+		g2.translate(-this.x,-this.y);
+	}
+	
+	public void update(Hero hero) {
+		if(hero.x<this.x) {
+			this.moveLeft();;	
+		}
+		else if(hero.x>this.x) {
+			this.moveRight();
+		}
+		if(hero.getY()>this.y) {
+			this.jump();
+		}
 	}
 }
